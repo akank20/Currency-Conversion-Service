4d0b6bf173af807756af73748ef2e0d1a8ba322f
akank20 <akanksha@Akankshas-MacBook-Pro.local>
2020-01-22 21:04:03 -0800

    adding other services

---
+++ currency-conversion-service/src/main/java/com/demo/microservices/CurrencyConversionServiceApplication.java
@@ -0,0 +1,26 @@
+package com.demo.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableFeignClients("com.demo.microservices")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+
+}
---
+++ currency-exchange-service/src/main/java/com/demo/microservices/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.demo.microservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	ExchangeValue findByFromAndTo(String from, String to);
+}
